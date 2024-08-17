b6f775d1ba4082ec127685b7977a0f48e024032f
saeed rastegar <saeed.rastegarrazaghi@tu-clausthal.de>
2024-08-17 12:42:09 +0200

    Loading Data and Entity

--- src/main/java/com/example/spring_boot_rest/controller/JobRestController.java
+++ src/main/java/com/example/spring_boot_rest/controller/JobRestController.java
@@ -1,46 +1,58 @@
 package com.example.spring_boot_rest.controller;
 
-import com.example.spring_boot_rest.model.JobPost;
-import com.example.spring_boot_rest.service.JobService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
-@RestController // if it is Controller, it means that i want to return view name but in this case i don't want to return view name but data, there for i use RestController
-@CrossOrigin(origins ="http://localhost:3000") // this annotation allows frontend to access the backend
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.spring_boot_rest.model.JobPost;
+import com.example.spring_boot_rest.service.JobService;
+
+@RestController
 public class JobRestController {
 
     @Autowired
     private JobService service;
 
-    @GetMapping(path="jobPosts", produces = {"application/json"})
-    // @ResponseBody oder instead of @Controller i can use @RestController
+    @GetMapping("jobPosts")
     public List<JobPost> getAllJobs() {
         return service.getAllJobs();
     }
 
-    @GetMapping("jobPost/{postId}")
+    @GetMapping("/jobPost/{postId}")
     public JobPost getJob(@PathVariable int postId) {
         return service.getJob(postId);
     }
 
-    @PostMapping(path="jobPost", consumes = {"application/xml"})
-    public JobPost addJob(@RequestBody JobPost jobPost){
+    @PostMapping("jobPost")
+    public JobPost addJob(@RequestBody JobPost jobPost) {
         service.addJob(jobPost);
         return service.getJob(jobPost.getPostId());
     }
 
     @PutMapping("jobPost")
-    public JobPost updateJob(@RequestBody JobPost jobPosts){
-        service.updateJob(jobPosts);
-        return service.getJob(jobPosts.getPostId());
+    public JobPost updateJob(@RequestBody JobPost jobPost) {
+        service.updateJob(jobPost);
+        return service.getJob(jobPost.getPostId());
     }
 
     @DeleteMapping("jobPost/{postId}")
-    public String deleteJob(@PathVariable int postId) {
+    public String deleteJob(@PathVariable int postId)
+    {
         service.deleteJob(postId);
-        return "deleted";
+        return "Deleted";
+    }
+
+    @GetMapping("load")
+    public String loadData() {
+        service.load();
+        return "success";
     }
 
 }
--- src/main/java/com/example/spring_boot_rest/model/JobPost.java
+++ src/main/java/com/example/spring_boot_rest/model/JobPost.java
@@ -1,19 +1,29 @@
 package com.example.spring_boot_rest.model;
 
+import java.util.List;
+
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Component
+@Entity
 public class JobPost {
+
+    @Id
     private int postId;
     private String postProfile;
     private String postDesc;
     private Integer reqExperience;
+    @ElementCollection
     private List<String> postTechStack;
 }
